078d5e5434c3dc0ef7b5057023e07a89f668aaee
Gauthier <gauthier.baudelet@student.uclouvain.be>
2026-05-26 08:46:51 +0200

    Initial Commit

---
+++ StudentHub/StudentHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentHubApp.swift
+//  StudentHub
+//
+//  Created by Gauthier Baudelet on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudentHubTests/StudentHubTests.swift
@@ -0,0 +1,18 @@
+//
+//  StudentHubTests.swift
+//  StudentHubTests
+//
+//  Created by Gauthier Baudelet on 5/26/26.
+//
+
+import Testing
+
+struct StudentHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
